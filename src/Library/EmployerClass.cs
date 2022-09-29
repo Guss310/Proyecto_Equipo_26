c2f8780692c3be13800920a12fa557c80bb52f0e
ffrancodl <104432558+ffrancodl@users.noreply.github.com>
2022-09-29 16:16:22 -0300

    Update EmployerClass.cs

--- src/Library/EmployerClass.cs
+++ src/Library/EmployerClass.cs
@@ -1,5 +1,41 @@
 namespace Library;
 public class Employer
 {
-  {  
+  public string PlatformReg
+    
+  { 
+    //Registrarse en plataforma.
+  }
+  
+  {
+    public string ResultadoBusquedaUbi
+  }
+  
+  {
+    //Ver el resultado de las busquedas de ofertas de trabajo, ordenado en forma ascendente de distancia a mi ubicación.
+  }
+  
+  {
+    public string ResultadoBusquedaRep
+  }
+  
+  {
+    //Ver el resultado de busquedas de ofertas de trabajo, ordenado en forma descendente por reputación.
+  }
+  
+  {
+    public string ContactarTrabajador
+  }
+  
+  {
+    //Contactar aun trabajador para contratar su oferta de servicio.
+  }
+  
+  {
+    public string CalificarTrabajador
+  }
+  
+  {
+    //Calificar a un trabajador
+  }
 }
