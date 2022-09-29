c8b47cb29ce182bcc27b92bb4859d5af21f9da94
Guss310 <105060468+Guss310@users.noreply.github.com>
2022-09-29 16:13:30 -0300

    Update WorkerClass.cs

--- src/Library/WorkerClass.cs
+++ src/Library/WorkerClass.cs
@@ -1,5 +1,14 @@
 namespace Library;
-public class Worker
 {
-
+  public class Administrador
+  {
+    public string IndicarCategoria
+    {
+      // Indicar categorías sobre las cuales se realizarán las ofertas de servicio para que de esa forma, los trabajadores puedan clasificarlos.
+    }
+    public string BajaServicio
+    {
+      // Dar de baja ofertas de servicios, avisando al oferente para que de esa forma, pueda evitar ofertas inadecudas.
+    }
+  }
 }
